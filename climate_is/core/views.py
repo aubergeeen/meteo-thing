5b563daedf204e35f7c8ef6e265b962ff6859e3a
tvaiababushka <91333188+tvaiababushka@users.noreply.github.com>
2025-06-16 19:16:35 +0500

    Подчистила шаблоны

--- climate_is/core/views.py
+++ climate_is/core/views.py
@@ -5,26 +5,6 @@ from stations.models import ParameterType
 import datetime
 from django.utils import timezone
 
-# def thingy(request):
-#     template = loader.get_template('core/dash.html')
-#     return HttpResponse(template.render())
-
-# def table_thing(request):
-#     template = loader.get_template('core/temp_table.html')
-#     return HttpResponse(template.render())
-
-# def choropleth(request):
-#     data = ParameterType.objects.all().values('param_id', 'name')  # получаем поля id и name всех станций из таблицы
-#     #template = loader.get_template('core/choropleth.html')
-#     return render(request, 'core/choropleth.html', {'params': data})
-#     #return HttpResponse(template.render())
-
-# def graph(request):
-#     #data = ParameterType.objects.all().values('param_id', 'name')  # получаем поля id и name всех станций из таблицы
-#     template = loader.get_template('core/graphs.html')
-#     #return render(request, 'core/choropleth.html', {'params': data})
-#     return HttpResponse(template.render())
-
 def dud(request):
     template = loader.get_template('core/dud.html')
     return HttpResponse(template.render())
